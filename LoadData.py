5f5f8413694012eb0e4fd4eade39c73b96d03b37
RodrigoArroyo4 <rodrigoarroyo4@hotmail.com>
2019-10-25 13:23:25 -0500

    fixed normalized method

--- LoadData.py
+++ LoadData.py
@@ -4,30 +4,47 @@ import pandas as pd
 col_names = list(range(1,501))
 filename = "madelon_train.data"
 df = pd.read_csv(filename, sep=' ', names=col_names, index_col=False, header=None)
-print(df)
+#print(df)
 
-def get_min_value_of_df():
+def get_min_value_of_df(data):
     global df
-    maxVals = df.max(axis = 1)
-    return maxVals
-    print(maxVals.max())
-
-def get_max_norm_of_df():
-    global df
-    minVals = df.min(axis = 1)
+    minVals = data.min(axis = 1)
+    minVals = minVals.min()
+    print("Min:")
+    print(minVals)
     return minVals
-    print(minVals.min())
+
+def get_max_norm_of_df(data):
+    global df
+    maxVals = data.max(axis = 1)
+    maxVals = maxVals.min()
+    print("Max:")
+    print(maxVals)
+    return maxVals
 
 def normalize(dataframe):
-    normalized_df = (dataframe - get_min_value_of_df()) / (get_max_norm_of_df() - get_min_value_of_df())
-    return(normalized_df)
+    dataframe = dataframe[(dataframe < 600).all(axis=1)]
+    result = dataframe
+    print("Data Frame:")
+    print(result)
+    max_value = get_max_norm_of_df(result)
+    min_value = get_min_value_of_df(result)
+
+    for feature_name in df.columns:
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+
+    result.to_csv(r'./results.csv')
+    return result
 
 def covarianza():
     global df
 
     x = normalize(df)
     z = x.corr()
+    print("Data Frame Normalized:")
     print(x)
-    #print(z.nlargest(10,1)[1])
+    print("Data Frame Correlated:")
+    print(z)
+    print(z.nlargest(10,1)[1])
 
-#covarianza()
+covarianza()
