11385d03f93a92716d6f9ac786e6e3ab8525e3e6
AS <salazar.s.aaron@gmail.com>
2019-10-29 23:19:04 -0500

    f

--- LoadData.py
+++ LoadData.py
@@ -16,7 +16,7 @@ def get_min_value_of_df(data):
 
 def get_max_norm_of_df(data):
     global df
-    maxVals = data.max(axis = 1)
+    maxVals = data.max(axis=1)
     maxVals = maxVals.max()
     print("Max:")
     print(maxVals)
@@ -25,8 +25,8 @@ def get_max_norm_of_df(data):
 def normalize(dataframe):
     #dataframe = dataframe[(dataframe < 650).all(axis=1)]
     result = dataframe
-    print("Data Frame:")
-    print(result)
+    # print("Data Frame:")
+    # print(result)
     max_value = get_max_norm_of_df(result)
     min_value = get_min_value_of_df(result)
 
@@ -36,6 +36,7 @@ def normalize(dataframe):
     result.to_csv(r'./results.csv')
     return result
 
+
 def covarianza():
     global df
     largest = []
@@ -43,25 +44,41 @@ def covarianza():
     x = normalize(df)
     z = x.corr(method='pearson')
     z.to_csv(r'./correlated.csv')
-    print("Data Frame Normalized:")
-    print(x)
-    print("Data Frame Correlated:")
-    print(z)
+    # print("Data Frame Normalized:")
+    # print(x)
+    # print("Data Frame Correlated:")
+    # print(z)
 
     tuple_list = []
     print("Largest Values: ")
     for i in range(1, 500):
         for j in range(1, 500):
-            if abs(z.at[i,j]) > 0.6 and z.at[i,j] != 1:
-                if z.at[i,j] not in largest:
+            if 0.1 < abs(z.at[i, j]) < 0.6 and z.at[i,j] != 1:
+                if z.at[i, j] not in largest:
                     tuple_list.append((i,j, z.at[i,j]))
                     largest.append(z.at[i,j])
 
     # we have a list of tuples with the format (Column, row, value)
-    # the column and row can be interchangable
+    # the column and row can be interchangeable
     print(len(largest))
+    print(len(tuple_list))
     print(tuple_list)
+    #res_dictionary = {}
+   # for col, row, val in tuple_list:
+       # if col in res_dictionary.keys():
+       #     res_dictionary[col].append(row)
+       # else:
+        #    res_dictionary[col] = []
+    # remove_cols([319, 443, 452, 473, 379, 337, 456, 476, 129, 339, 282, 434, 242, 494])
 
+
+def remove_cols(int_array):
+    global df
+
+    for val in int_array:
+        df = df.drop(val, 1)
+    # df.to_csv(r'./df_after_first_corr.csv')
+    print(df)
 
 
 covarianza()
